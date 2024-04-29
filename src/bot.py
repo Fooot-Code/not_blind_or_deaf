4f52b95979bf42bc596a2e4ff2761723f8cdaa5b
Fooot <59772241+Fooot-Code@users.noreply.github.com>
2024-04-28 19:34:53 -0600

    Added It
    Should work but isn't tested

--- src/bot.py
+++ src/bot.py
@@ -12,6 +12,7 @@ from rlutilities.linear_algebra import vec3, norm, normalize, clip
 
 from get_to_air_point import GetToAirPoint
 from kickoff import Kickoff
+import time
 
 HOVER_IDLE_HEIGHT = 2000
 HOVER_IDLE_Y = 1000
@@ -19,6 +20,10 @@ HOVER_MAX_HEIGHT = 1800
 HOVER_MAX_SIDE = 3900
 HOVER_TARGET_Y = 5000
 GET_TO_DISTANCE = 1000 # farthest you can be to get to the ball
+CHECKING_TIME = 6000 # needs to be adjusted; is the time (in miliseconds) needed to elapse to check ball movement
+
+starting_time = time.time()
+list_of_ball_positions = [] # in vectors (?)
 
 # Team Blue is 0, Team Orange is 1
 
@@ -51,8 +56,7 @@ class MyBot(BaseAgent):
 
         self.sign = 2 * self.team - 1  # 1 if orange, else -1
 
-    def get_output(self, packet: GameTickPacket) -> SimpleControllerState: 
-        self.ball = packet.game_ball
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         self.info.read_packet(packet)
         if self.car is None:
             # Inits car and stuff
@@ -67,13 +71,15 @@ class MyBot(BaseAgent):
             
             return self.controls
         
+        time_now = time.time()
+        time_elapsed = time_now - starting_time
+        
         # Gets the ball prediction and converts it to a vector
         ball_prediction = self.get_ball_prediction_struct()
-        go_to_balls_y = self.get_vec3_ball(ball_prediction)
+        go_to_balls_y = self.get_vec3_ball(ball_prediction, time_elapsed)
         
         target = go_to_balls_y 
         
-
         # Render target and ball prediction
         polyline = [ball_prediction.slices[i].physics.location for i in range(0, 100, 5)]
         self.renderer.draw_polyline_3d(polyline, self.renderer.yellow())
@@ -87,12 +93,23 @@ class MyBot(BaseAgent):
 
         return self.controls
 
-    def get_vec3_ball(self, ball_prediction):
+    def get_vec3_ball(self, ball_prediction, time_elapsed):
         for step in ball_prediction.slices[:ball_prediction.num_slices]:
             pos = step.physics.location
+            
+            if len(list_of_ball_positions) <= 1 and time_elapsed >= CHECKING_TIME:
+                list_of_ball_positions.append(self.info.ball.position)
 
-            # Moves to ball if close enough
-            if dist(self.info.ball.position, self.car.position) <= GET_TO_DISTANCE: 
-                return vec3(pos.x, 0, pos.z if pos.z > self.hover_min_height else self.hover_min_height)
-            else: 
-                return vec3(0, 0, self.hover_min_height) # hovers if not
+            elif len(list_of_ball_positions) >= 2: # realistically, this should never go over 2
+                distance_from_ball = dist(list_of_ball_positions[0], list_of_ball_positions[1])
+                movement_vector = vec3(pos.x, 0, pos.z if pos.z > self.hover_min_height else self.hover_min_height)
+                hover_vector = vec3(0, 0, self.hover_min_height)
+
+                # return movement_vector if distance_from_ball != abs(distance_from_ball) 
+                if (self.team == 0 and distance_from_ball != abs(distance_from_ball)) or \
+                    (self.team != 0 and distance_from_ball != abs(distance_from_ball) and self.info.ball.position[1] >= 0):
+                    return movement_vector
+                
+                elif (self.team == 0 and distance_from_ball == abs(distance_from_ball) and self.info.ball.position[1] <= 0) or \
+                    (self.team != 0 and distance_from_ball == abs(distance_from_ball)):
+                    return hover_vector
